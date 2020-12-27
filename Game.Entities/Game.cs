63ea25432673fc515843b2dca1427a7175b95794
Jamie Fraser <jamie.fraser@imason.com>
2020-12-27 17:23:08 -0500

    Create Game

--- Game.Entities/Game.cs
+++ Game.Entities/Game.cs
@@ -2,20 +2,38 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace Game.Entities
 {
     public class Game : Microsoft.Azure.Cosmos.Table.TableEntity
     {
+        [Required]
+        [Display(Name ="How Many Decks?")]
         public int NumberOfDecks { get; set; }
+        [Required]
+        [EnumDataType(enumType:typeof(DeckType), ErrorMessage = "Please select a valid deck type")]
         public DeckType DeckType { get; set; }
+        [Display(Name ="Minimum Number of Players")]
+        [Required]
         public int MinimumPlayers { get; set; }
+        [Required()]
+        [Display(Name ="Maximum Number of Players")]
         public int MaxPlayers { get; set; }
+        [Required]
+        [Display(Name ="Use a Discard Pile?")]
         public bool UsesDiscardPile { get; set; }
+        [Display(Name ="Pick up Whole Discard Pile?")]
         public bool PlayerPicksUpWholeDiscardPile { get; set; }
+        [Display(Name ="How Many Cards in a Hand?")]
+        [Required]
         public int NumberOfCardsToDeal { get; set; }
-
+        [Display(Name ="Each Hand Gets More Cards?")]
         public bool ProgressiveDeal { get; set; }
+        [Display(Name ="How Many More Cards per Hand?")]
         public int IncrementCardsToDealBy { get; set; }
+        [Required]
+        [Display(Name ="Name")]
+        public new string RowKey { get; set; }
     }
 }
---
+++ Game.Entities/GreaterThanZeroValidator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Game.Entities
+{
+    public class GreaterThanZeroValidator : ValidationAttribute
+    {
+        public static ValidationResult IsGreaterThanZero(DeckType value)
+        {
+            ValidationResult result = new ValidationResult(((DeckType)value) > 0 ? null : "Not greater than zero");
+            return result ; 
+        }
+    }
+}
