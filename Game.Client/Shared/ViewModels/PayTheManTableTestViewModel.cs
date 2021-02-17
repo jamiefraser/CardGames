f33ccdd2fb65e584970e5ead6d394df2a0c12b42
Jamie Fraser <jamie.fraser@imason.com>
2021-02-17 06:50:57 -0500

    Basic rendering of phase 10 cards with CSS sorted.  Still have to figure out how to render the other players and their hands

--- Game.Client/Shared/ViewModels/PayTheManTableTestViewModel.cs
+++ Game.Client/Shared/ViewModels/PayTheManTableTestViewModel.cs
@@ -13,7 +13,7 @@ namespace Game.Client.Shared.ViewModels
         {
             Hand = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Card>>(handjson);
         }
-        private string handjson = "[{'IsSpecialCard':false,'Suit':'red','Rank':1},{'IsSpecialCard':false,'Suit':'red','Rank':3},{'IsSpecialCard':false,'Suit':'green','Rank':4},{'IsSpecialCard':false,'Suit':'blue','Rank':4},{'IsSpecialCard':false,'Suit':'red','Rank':5},{'IsSpecialCard':false,'Suit':'red','Rank':10},{'IsSpecialCard':false,'Suit':'red','Rank':7},{'IsSpecialCard':false,'Suit':'green','Rank':4},{'IsSpecialCard':false,'Suit':'red','Rank':9},{'IsSpecialCard':false,'Suit':'wild','Rank':100},{'IsSpecialCard':false,'Suit':'red','Rank':3},{'IsSpecialCard':false,'Suit':'blue','Rank':8},{'IsSpecialCard':false,'Suit':'yellow','Rank':3}]";
+        private string handjson = "[{'IsSpecialCard':false,'Suit':'red','Rank':1},{'IsSpecialCard':false,'Suit':'red','Rank':11},{'IsSpecialCard':false,'Suit':'green','Rank':4},{'IsSpecialCard':false,'Suit':'blue','Rank':4},{'IsSpecialCard':false,'Suit':'red','Rank':5},{'IsSpecialCard':false,'Suit':'red','Rank':10},{'IsSpecialCard':false,'Suit':'red','Rank':7},{'IsSpecialCard':false,'Suit':'green','Rank':4},{'IsSpecialCard':false,'Suit':'red','Rank':9},{'IsSpecialCard':false,'Suit':'wild','Rank':100},{'IsSpecialCard':false,'Suit':'skip','Rank':200},{'IsSpecialCard':false,'Suit':'blue','Rank':8},{'IsSpecialCard':false,'Suit':'yellow','Rank':3}]";
         public List<Player> Players { get; set; }
         public List<Card> Hand { get; set; }
     }
